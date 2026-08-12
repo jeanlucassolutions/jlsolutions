55853194ef65fc3e051d12f140aad8d7a10479d7
jeanlucassolutions <jeanlucas.solutions@gmail.com>
2026-08-11 23:04:05 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,21 +37,8 @@ export const metadata: Metadata = {
     locale: 'pt_BR',
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+  icon: '/jl-favicon-512.png',
+  apple: '/jl-favicon-512.png',
   },
 }
 
